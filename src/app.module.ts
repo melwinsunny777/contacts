3dd09ee1e789e9f6a1a5cb0cc0fc0761e0bcf3ef
melwinsunny777 <melwinsunny4627@gmail.com>
2023-10-16 14:19:23 +0530

    added separate for contacts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactsModule } from './contacts-backed/contacts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [ 
+    ConfigModule.forRoot({
+       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  }),
+  ScheduleModule.forRoot(),
+  ContactsModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
